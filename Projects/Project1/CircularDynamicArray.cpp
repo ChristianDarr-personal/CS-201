81c8b1daff89b4873045d9c94a7d80230834de10
Christian Darr <christianrdarr@gmail.com>
2022-09-14 04:00:11 +0100

    feat: constructors, simple methods
    Signed-off-by: Christian Darr <christianrdarr@gmail.com>

--- Projects/Project1/CircularDynamicArray.cpp
+++ Projects/Project1/CircularDynamicArray.cpp
@@ -5,21 +5,25 @@ using namespace std;
 template <class elmtype>
 class CircularDynamicArray{
     private:
-        elmtype data;
         int size;
         int capacity;
         int front;
         int end;
-        elmtype array[];
+        elmtype *array;
+        void grow();
+        void shrink();
     public:
+        CircularDynamicArray();
         CircularDynamicArray(int s);
         ~CircularDynamicArray();
 
-        void AddFront(elmtype v){};
-        void AddEnd(elmtype v){};
+        elmtype& operator[](int i);
 
-        void DelFront(){};
-        void DelEnd(){};
+        void AddFront(elmtype v);
+        void AddEnd(elmtype v);
+
+        void DelFront();
+        void DelEnd();
 
         int Length();
         int Capacity();
@@ -35,41 +39,74 @@ class CircularDynamicArray{
 };
 
 template <class elmtype>
+CircularDynamicArray<elmtype>::CircularDynamicArray(){
+    size = 0;
+    capacity = 2;
+    end = 0;
+    front = 0;
+    array = new elmtype[this->size];
+}
+
+template <class elmtype>
 CircularDynamicArray<elmtype>::CircularDynamicArray(int s){
+    size = s;
+    capacity = s;
+    front = 0;
+    end = this->size - 1;
+    array = new elmtype[this->size];
 }
 
 template <class elmtype>
 CircularDynamicArray<elmtype>::~CircularDynamicArray(){
 }
 
+
+template <class elmtype>
+elmtype& CircularDynamicArray<elmtype>:: operator[](int i){
+    if(i > size || i < 0) {
+        cout << "Array index is out of bounds";
+    } else {
+        return array[(front + i)%(size+1)];
+    }
+}
+
 template <class elmtype>
 void CircularDynamicArray<elmtype>::AddFront(elmtype v){
-
+    size += 1;
+    front = (front - 1)%(size+1);
+    array[front] = v;
 }
 
 template <class elmtype>
 void CircularDynamicArray<elmtype>::AddEnd(elmtype v){
-
+    size += 1;
+    if(size > capacity) {
+        capacity
+    }
+    end = (end + 1)%(size+1);
+    array[end] = v;
 }
 
 template <class elmtype>
 void CircularDynamicArray<elmtype>::DelFront(){
-
+    front = (front + 1)%(size+1);
+    size -= 1;
 }
 
 template <class elmtype>
 void CircularDynamicArray<elmtype>::DelEnd(){
-
+    end = (end - 1)%(size+1);
+    size -= 1;
 }
 
 template <class elmtype>
 int CircularDynamicArray<elmtype>::Length(){
-
+    return size;
 }
 
 template <class elmtype>
 int CircularDynamicArray<elmtype>::Capacity(){
-
+    return capacity;
 }
 
 template <class elmtype>
