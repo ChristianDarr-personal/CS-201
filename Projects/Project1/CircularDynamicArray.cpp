b78bf9bd48aa06a4b2b299c43f09e946be9bd332
Christian Darr <christianrdarr@gmail.com>
2022-09-15 01:26:24 -0500

    refactor: inline declarations
    Signed-off-by: Christian Darr <christianrdarr@gmail.com>

--- Projects/Project1/CircularDynamicArray.cpp
+++ Projects/Project1/CircularDynamicArray.cpp
@@ -10,38 +10,120 @@ class CircularDynamicArray{
         int front;
         int end;
         elmtype *array;
-        void grow();
-        void shrink();
-        void print();
-    public:
-        CircularDynamicArray();
-        CircularDynamicArray(int s);
-        ~CircularDynamicArray();
 
-        CircularDynamicArray(const CircularDynamicArray& c);
-        CircularDynamicArray& operator=(const CircularDynamicArray& c){
-            this->size = c.size;
-            this->capacity = c.capacity;
-            this->front = c.front;
-            this->end = c.end;
-            elmtype *b = new elmtype[capacity];
-            for(int i = 0; i < size; i++){
-                b[i] = c[i];
+        void grow(){
+            capacity *= 2;
+            elmtype* temp = new elmtype[capacity];
+            for(int j = 0; j < size; j++)
+            {
+                temp[j] = array[(front + j)%capacity];
+                cout << temp[j] << endl;
             }
-            this->array = b;
-            cout << "Copy operator Called" << endl;
+            array = new elmtype[capacity];
+            for(int i = 0; i < size; i++)
+            {
+                array[i] = temp[i];
+            }
+            // delete[] temp;
+            front = 0;
+            end = size -1;
+            cout << "Doubling to: " << capacity << endl;
         }
 
-        elmtype& operator[](int i);
+        void shrink();
+        void print(){
+            for (int i=0; i< size;i++) cout << array[i] << endl;        
+        }
 
-        void AddFront(elmtype v);
-        void AddEnd(elmtype v);
+        void deepCopy(const CircularDynamicArray& c){
+            size = c.size;
+            capacity = c.capacity;
+            front = c.front;
+            end = c.end;
+            array = new elmtype[capacity];
+            for(int i = 0; i < size; i++){
+                array[i] = c[i];
+            }
+            cout << "deepCopy called" << endl << flush;
+        }
+    public:
+        CircularDynamicArray(){
+            size = 0;
+            capacity = 2;
+            end = 0;
+            front = 0;
+            array = new elmtype[this->size];
+        }
 
-        void DelFront();
-        void DelEnd();
+        CircularDynamicArray(int s){
+            size = s;
+            capacity = s;
+            front = 0;
+            end = this->size - 1;
+            array = new elmtype[this->size];
+        }
 
-        int Length();
-        int Capacity();
+        ~CircularDynamicArray(){
+            cout << "Constructor Called" << flush << endl;
+            delete[] array;
+        }
+
+        CircularDynamicArray(const CircularDynamicArray& c){
+            deepCopy(c);
+        }
+
+        CircularDynamicArray& operator=(const CircularDynamicArray& c){
+            if(this != &c){
+                delete[] array;
+                deepCopy(c);
+            }
+            return *this;
+        }
+
+        elmtype& operator[](int i){
+            if(i > size || i < 0) {
+                cout << "Array index is out of bounds";
+            } else {
+                return array[(front + i)%capacity];
+            }
+        }
+
+        void AddFront(elmtype v){
+            size += 1;
+            if(size > capacity){
+                grow();
+            }
+            front = (front - 1)%(capacity);
+            array[front] = v;
+        }
+
+
+        void AddEnd(elmtype v){
+            size += 1;
+            if(size > capacity) {
+                capacity;
+            }
+            end = (end + 1)%(capacity);
+            array[end] = v;
+        }
+
+        void DelFront(){
+            front = (front + 1)%(capacity);
+            size -= 1;      
+        }
+        void DelEnd(){
+            end = (end - 1)%(capacity);
+            size -= 1;
+        }
+
+        int Length(){
+            return size;
+        }
+
+        int Capacity()
+        {
+            return capacity;
+        }
 
         void Clear();
         
@@ -49,150 +131,26 @@ class CircularDynamicArray{
         elmtype WCSelect(int k);
 
         void StableSort();
-        int Search(elmtype v);
-        int BinSearch(elmtype v);
+        int Search(elmtype v){
+            for(int i = 0; i < size; i++){
+                if(array[i] == v){
+                    return i;
+                }
+            }
+        }
+        int BinSearch(elmtype v){
+            int left = 0, right = size, center;   
+            while (left <= right) {
+                center = (left + right) / 2;
+                if(array[center] == v) {
+                    return center;
+                }
+                if(array[center] < v) {
+                    left = center + 1;
+                } else {
+                    right = center -1;
+                }
+                return -1;
+            }
+        }
 };
-
-template <class elmtype>
-void CircularDynamicArray<elmtype>::grow(){
-    capacity *= 2;
-    elmtype *b;
-    b = new elmtype[size];
-    b = array;
-    array = new elmtype[capacity];
-    for(int i = 0; i < size - 1; i++)
-    {
-        array[i] = b[i];
-    }
-    delete[] b;
-}
-
-template <class elmtype>
-void CircularDynamicArray<elmtype>::print(){
-    for (int i=0; i< size;i++) cout << array[i] << endl;
-}
-template <class elmtype>
-void CircularDynamicArray<elmtype>::shrink(){
-
-}
-template <class elmtype>
-CircularDynamicArray<elmtype>::CircularDynamicArray(){
-    size = 0;
-    capacity = 2;
-    end = 0;
-    front = 0;
-    array = new elmtype[this->size];
-}
-
-template <class elmtype>
-CircularDynamicArray<elmtype>::CircularDynamicArray(int s){
-    size = s;
-    capacity = s;
-    front = 0;
-    end = this->size - 1;
-    array = new elmtype[this->size];
-}
-
-template <class elmtype>
-CircularDynamicArray<elmtype>::~CircularDynamicArray(){
-    delete[] array;
-    cout << "Destructor Called" << endl;
-}
-
-
-template <class elmtype>
-elmtype& CircularDynamicArray<elmtype>:: operator[](int i){
-    if(i > size || i < 0) {
-        cout << "Array index is out of bounds";
-    } else {
-        return array[(front + i)%capacity];
-    }
-}
-
-template <class elmtype>
-void CircularDynamicArray<elmtype>::AddFront(elmtype v){
-    size += 1;
-    if(size > capacity){
-        grow();
-    }
-    front = (front - 1)%(capacity);
-    array[front] = v;
-}
-
-template <class elmtype>
-void CircularDynamicArray<elmtype>::AddEnd(elmtype v){
-    size += 1;
-    if(size > capacity) {
-        capacity;
-    }
-    end = (end + 1)%(capacity);
-    array[end] = v;
-}
-
-template <class elmtype>
-void CircularDynamicArray<elmtype>::DelFront(){
-    front = (front + 1)%(capacity);
-    size -= 1;
-}
-
-template <class elmtype>
-void CircularDynamicArray<elmtype>::DelEnd(){
-    end = (end - 1)%(capacity);
-    size -= 1;
-}
-
-template <class elmtype>
-int CircularDynamicArray<elmtype>::Length(){
-    return size;
-}
-
-template <class elmtype>
-int CircularDynamicArray<elmtype>::Capacity(){
-    return capacity;
-}
-
-template <class elmtype>
-void CircularDynamicArray<elmtype>::Clear(){
-
-}
-
-template <class elmtype>
-elmtype CircularDynamicArray<elmtype>::QuickSelect(int k){
-
-}
-
-template <class elmtype>
-elmtype CircularDynamicArray<elmtype>::WCSelect(int k){
-
-}
-
-template <class elmtype>
-void CircularDynamicArray<elmtype>::StableSort(){
-
-}
-
-template <class elmtype>
-int CircularDynamicArray<elmtype>::Search(elmtype v){
-    for(int i = 0; i < size; i++){
-        if(array[i] == v){
-            return i;
-        }
-    }
-}
-
-template <class elmtype>
-int CircularDynamicArray<elmtype>::BinSearch(elmtype v){
-    int left = 0, right = size, center;   
-    while (left <= right) {
-        center = (left + right) / 2;
-        if(array[center] == v) {
-            return center;
-        }
-        if(array[center] < v) {
-            left = center + 1;
-        } else {
-            right = center -1;
-        }
-        return -1;
-    }
-}
