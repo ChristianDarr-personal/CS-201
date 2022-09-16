7ff262605c360f18deca0b50483080ccf77284ae
Christian Darr <christianrdarr@gmail.com>
2022-09-15 23:23:16 -0500

    fix: change function names to match main
    Signed-off-by: Christian Darr <christianrdarr@gmail.com>

--- Projects/Project1/CircularDynamicArray.cpp
+++ Projects/Project1/CircularDynamicArray.cpp
@@ -6,31 +6,26 @@ template <class elmtype>
 class CircularDynamicArray{
     private:
         int size;
-        int capacity;
+        int cap;
         int front;
         int end;
         elmtype *array;
         elmtype *bad;
 
         void grow(){
-            capacity *= 2;
-            elmtype* temp = new elmtype[capacity];
+            elmtype* temp = new elmtype[cap*2];
             for(int j = 0; j < size; j++)
             {
-                temp[j] = array[(front + j)%capacity];
+                temp[j] = array[(front + j)%cap];
                 // cout << temp[j] << endl;
             }
-            delete[] array;
-            array = temp;
-            // new elmtype[capacity];
-            // for(int i = 0; i < size; i++)
-            // {
-            //     array[i] = temp[i];
-            // }
-            delete[] temp;
+            
+            this->array = temp;
+            cap *=2;
             front = 0;
             end = size -1;
-            cout << "Doubling to: " << capacity << endl;
+            cout << "Doubling to: " << cap << endl;
+            // delete[] ptDelete;
         }
 
         void shrink();
@@ -38,33 +33,34 @@ class CircularDynamicArray{
             for (int i=0; i< size;i++) cout << array[i] << endl;        
         }
 
-        void deepCopy(const CircularDynamicArray& c){
-            size = c.size;
-            capacity = c.capacity;
-            front = c.front;
-            end = c.end;
-            array = new elmtype[capacity];
+        void deepCopy(const CircularDynamicArray &c){
+            this->size = c.size;
+            this->cap = c.cap;
+            this->front = c.front;
+            this->end = c.end;
+            this->array = new elmtype[cap];
             for(int i = 0; i < size; i++){
-                array[i] = c[i];
+                array[i] = c.array[i];
             }
             cout << "deepCopy called" << endl << flush;
         }
+        void select()
 
     public:
         CircularDynamicArray(){
             size = 0;
-            capacity = 2;
+            cap = 2;
             end = 0;
             front = 0;
-            array = new elmtype[capacity];
+            array = new elmtype[cap];
         }
 
         CircularDynamicArray(int s){
             size = s;
-            capacity = s;
+            cap = s;
             front = 0;
             end = this->size - 1;
-            array = new elmtype[capacity];
+            array = new elmtype[cap];
         }
 
         ~CircularDynamicArray(){
@@ -90,75 +86,80 @@ class CircularDynamicArray{
                 cout << "Array index is out of bounds" << endl;
                 return *bad;
             } else {
-                return array[(front + i)%capacity];
+                return array[(front + i)%cap];
             }
         }
 
-        void AddFront(elmtype v){
+        void addFront(elmtype v){
             size += 1;
-            if(size > capacity){
+            if(size > cap){
                 grow();
             }
-            front = (front - 1)%(capacity);
+            front = (front - 1)%(cap);
             array[front] = v;
         }
 
 
-        void AddEnd(elmtype v){
+        void addEnd(elmtype v){
             size += 1;
-            if(size > capacity) {
-                capacity;
+            if(size > cap) {
+                cap;
             }
-            end = (end + 1)%(capacity);
+            end = (end + 1)%(cap);
             array[end] = v;
         }
 
-        void DelFront(){
-            front = (front + 1)%(capacity);
+        void delFront(){
+            front = (front + 1)%(cap);
             size -= 1;      
         }
-        void DelEnd(){
-            end = (end - 1)%(capacity);
+        void delEnd(){
+            end = (end - 1)%(cap);
             size -= 1;
         }
 
-        int Length(){
+        int length(){
             return size;
         }
 
-        int Capacity(){
-            return capacity;
+        int capacity(){
+            return cap;
         }
 
-        void Clear(){
+        void clear(){
             delete[] array;
             array = new elmtype[2];
             size = 0;
-            capacity = 2;
+            cap = 2;
             front = 0;
             end = 0;
             cout << "clear" << flush << endl;
         }
         
-        elmtype QuickSelect(int k);
+        elmtype QuickSelect(int k){
+          
+        }
+
         elmtype WCSelect(int k);
 
-        void StableSort();
-        int Search(elmtype v){
+        void stableSort();
+
+        int linearSearch(elmtype v){
             for(int i = 0; i < size; i++){
-                if(array[i] == v){
+                if(array[array[(front + i)%cap] == v){
                     return i;
                 }
             }
         }
-        int BinSearch(elmtype v){
+
+        int binSearch(elmtype v){
             int left = 0, right = size, center;   
             while (left <= right) {
                 center = (left + right) / 2;
-                if(array[(front + center)%capacity] == v) {
+                if(array[(front + center)%cap] == v) {
                     return center;
                 }
-                if(array[(front + center)%capacity] > v) {
+                if(array[(front + center)%cap] > v) {
                     left = center + 1;
                 } else {
                     right = center -1;
