d7a974badf815e1bb8055908902e4f656bbafc06
Christian Darr <christianrdarr@gmail.com>
2022-09-18 02:02:30 -0500

    fix: QuickSelect
    Signed-off-by: Christian Darr <christianrdarr@gmail.com>

--- Projects/Project1/CircularDynamicArray.cpp
+++ Projects/Project1/CircularDynamicArray.cpp
@@ -8,7 +8,6 @@ class CircularDynamicArray{
         int size;
         int cap;
         int front;
-        int end;
         elmtype *array;
         elmtype *bad;
 
@@ -21,7 +20,6 @@ class CircularDynamicArray{
             array = temp;
             cap *=2;
             front = 0;
-            end = size - 1;
         }
 
         void shrink(){
@@ -40,11 +38,11 @@ class CircularDynamicArray{
             this->size = c.size;
             this->cap = c.cap;
             this->front = c.front;
+            this->bad = c.bad;
             this->array = new elmtype[cap];
             for(int i = 0; i < size; i++){
                 array[i] = c.array[i];
             }
-            cout << "deepCopy called" << endl << flush;
         }
 
         void mergeSort(elmtype* arr, int n){
@@ -54,18 +52,10 @@ class CircularDynamicArray{
             elmtype* left = new elmtype[n/2];
             elmtype* right = new elmtype[n - n/2];
             for(int i = 0; i < n/2; i++){
-                if(&arr == &array) {
-                    left[i] = arr[(front + i)%cap];
-                } else {
                     left[i] = arr[i];
-                }
             }
             for(int i = n/2; i < n; i++){
-                if(&arr == &array) {
-                    right[i-n/2] = arr[(front + i)%cap];
-                } else {
                     right[i-n/2] = arr[i];
-                }
             }
             mergeSort(left, n/2);
             mergeSort(right, n-n/2);
@@ -93,12 +83,82 @@ class CircularDynamicArray{
             }
         }
 
+        // elmtype selectWC(elmtype* arr, int k, int s){
+        //     int g = 5;
+
+        //     int l = 0, e = 0, g = 0;
+        //     elmtype* less = new elmtype[s];
+        //     elmtype* greater = new elmtype[s];
+        //     for(int i = 0; i < s; i++){
+        //         elmtype x;
+        //         x = arr[i];
+        //         if (x<pivot){
+        //                 less[l++] = x;
+        //         } else if (x == pivot){
+        //             e++;
+        //         } else{
+        //             greater[g++] = x;
+        //         }
+        //     }
+        //     if (k <= l){
+        //         return selectNormal(less, k, l, wc);
+        //     }
+        //     else if (k <= l + e){
+        //         return pivot; 
+        //     } else {
+        //         return selectNormal(greater, k - l - e, g);
+        //     }
+        // }
+
+        elmtype selectNormal(elmtype* arr, int k, int s, bool pass){
+            int pivot = arr[rand() % s];
+            int l = 0, e = 0, g = 0;
+            elmtype x;
+            elmtype* less = new elmtype[s];
+            elmtype* greater = new elmtype[s];
+            for(int i = 0; i < s; i++){
+                if(pass){
+                    x = arr[(i + front)%cap];
+                }else {
+                    x = arr[i];
+                }
+                if (x<pivot){
+                    less[l++] = x;
+                } else if (x == pivot){
+                    e++;
+                } else{
+                    greater[g++] = x;
+                }
+            }
+            if (k <= l){
+                return selectNormal(less, k, l, false);
+            }
+            else if (k <= l + e){
+                return pivot; 
+            } else {
+                return selectNormal(greater, k - l - e, g, false);
+            }
+        }
+
+        int search(elmtype x, int l, int r){
+            int c = (l + r) / 2;
+            if (l > r) {
+                return -1;
+            }
+            if(array[(front + c)%cap] == x) {
+                return c;
+            } else if(x < array[(front + c)%cap]) {
+                search(x, l, c - 1);
+            } else if(x > array[(front + c)%cap]){
+                search(x, c + 1, r);
+            }
+        }
+
     public:
         CircularDynamicArray(){
             size = 0;
             cap = 2;
             front = 0;
-            end = 0;
             array = new elmtype[cap];
         }
 
@@ -106,7 +166,6 @@ class CircularDynamicArray{
             size = s;
             cap = s;
             front = 0;
-            end = 0;
             array = new elmtype[cap];
         }
 
@@ -153,8 +212,7 @@ class CircularDynamicArray{
                 grow();
             }
             size += 1;
-            end += 1;
-            array[end] = v;
+            array[(front + (size - 1)%cap)] = v;
         }
 
         void delFront(){
@@ -186,39 +244,20 @@ class CircularDynamicArray{
             size = 0;
             cap = 2;
             front = 0;
-            end = 0;
             cout << "clear" << flush << endl;
         }
-        
-        // elmtype QuickSelect(int k){
-        //     int pivot = array[(front + 5)%(cap)];
-        //     int l, e, g;
-        //     for(int i = 0; i < size; i++){
-        //         int x = array[(front + i)%(cap)];
-        //         if (x<pivot){
-        //             l++;
-        //         } else if (x==pivot){
-        //             e++;
-        //         } else{
-        //             g++;
-        //         }
-        //     }
-        //     if (k <= l){
-        //         return Select (k);
-        //     }
-        //     else if (k <= l + e){
-        //         return pivot; 
-        //     } else {
-        //         return Select (k - l - e);
-        //     }
-        // }
 
-        elmtype WCSelect(int k);
+        elmtype QuickSelect(int k){
+            return selectNormal(array, k, size, true);
+        }
+
+        // elmtype WCSelect(int k){
+        //     return selectWC(array, k, size);
+        // }
 
         void stableSort(){
             mergeSort(array, size);
             front = 0;
-            end = size - 1;
         }
 
         int linearSearch(elmtype v){
@@ -227,26 +266,10 @@ class CircularDynamicArray{
                     return i;
                 }
             }
+            return -1;
         }
 
         int binSearch(elmtype v){
             return search(v, 0, size - 1);
         }
-
-        int search(elmtype x, int l, int r){
-            int c;
-            c = (l + r) / 2;
-            
-            if (l > r) {
-                return -1;
-            }
-            if(array[(front + c)%cap] == x) {
-                return c;
-            } else if(x < array[(front + c)%cap]) {
-                search(x, l, c - 1);
-            } else if(x > array[(front + c)%cap]){
-                search(x, c + 1, r);
-            }
-        }
 };
-
--- Projects/Project1/Phase1Main.cpp
+++ Projects/Project1/Phase1Main.cpp
@@ -11,74 +11,65 @@ void foo(CircularDynamicArray<int> x) {
 
 int main(){
 	CircularDynamicArray<float> C(10);
-	
 	for (int i=0; i< C.length();i++) C[i] = i;
 	for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-	for(int j = 10; j < 20; j++){
-		C.addFront(j);
-		for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-	}
-	C.stableSort();
-	
-	for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-	cout << "bin search" << C.binSearch(13.0) << endl;
-	cout << "lin search" << C.linearSearch(14.0) << endl;
-	int x = C.length();
-	for(int j =x; j < x*2; j++){
-		C.addFront(j);
-		for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-	}
-	
-	for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-
 	// C => "0 1 2 3 4 5 6 7 8 9"
-	// C.delFront();
-	// for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-	// // C => "1 2 3 4 5 6 7 8 9"
-	// C.delEnd();
-	// for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-	// // C => "1 2 3 4 5 6 7 8"
-	// C.addEnd(100.0);
-	// for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
-	// // C => "1 2 3 4 5 6 7 8 100"
-	// C.delFront();
-	// C.addEnd(200.0);
-	// // C => "2 3 4 5 6 7 8 100 200"	
+	C.delFront();
+	for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
+	// C => "1 2 3 4 5 6 7 8 9"
+	C.delEnd();
+	for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
+	// C => "1 2 3 4 5 6 7 8"
+	C.addEnd(100.0); //error here
+	for (int i=0; i< C.length();i++) cout << C[i] << " ";  cout << endl;
+	// C => "1 2 3 4 5 6 7 8 100"
+	C.delFront();
+	C.addEnd(200.0);
+	// C => "2 3 4 5 6 7 8 100 200"	
 
-	// C.addEnd(300.0);
-	// C.addEnd(400.0);
-	// // C => "2 3 4 5 6 7 8 100 200 300 400"	
+	C.addEnd(300.0);
+	C.addEnd(400.0);
+	// C => "2 3 4 5 6 7 8 100 200 300 400"	
 
-	// CircularDynamicArray<int> A,B;
-	// for(int i=0; i<10;i++) A.addEnd(i);
-	// B = A;
-	// A.addEnd(15); A.addEnd(19);
-	// // A => "0 1 2 3 4 5 6 7 8 9 15 19" 
-	// cout << "Select is " << A.linearSearch(5) << endl;
-	// // A => "0 1 2 3 4 5 6 7 8 9 15 19" Search => 5
-	// cout << "Select is " << A.binSearch(12) << endl;
-	// // A => "0 1 2 3 4 5 6 7 8 9 15 19" Search => -1
-	// cout << "Select is " << A.binSearch(15) << endl;
-	// // A => "0 1 2 3 4 5 6 7 8 9 15 19" Search => 10	
-	// A.addFront(10); 
-	// // A => "10 0 1 2 3 4 5 6 7 8 9 15 19"
-	// cout << "Select is " << A.linearSearch(5) << endl;
-	// // A => "10 0 1 2 3 4 5 6 7 8 9 15 19" Search => 6
-	// cout << "Select is " << A.QuickSelect(3) << endl;
-    // // Select => 2	
+	CircularDynamicArray<int> A,B;
+	for(int i=0; i<10;i++) A.addEnd(i);
+	B = A;
+	A.addEnd(15); A.addEnd(19);
+	// A => "0 1 2 3 4 5 6 7 8 9 15 19" 
+    for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
+
+	cout << "Select is " << A.linearSearch(5) << endl;
+	// A => "0 1 2 3 4 5 6 7 8 9 15 19" Search => 5
+	cout << "Select is " << A.binSearch(12) << endl;
+	// A => "0 1 2 3 4 5 6 7 8 9 15 19" Search => -1
+	cout << "Select is " << A.binSearch(15) << endl;
+	// A => "0 1 2 3 4 5 6 7 8 9 15 19" Search => 10	
+	A.addFront(10); 
+    for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
+
+	// A => "10 0 1 2 3 4 5 6 7 8 9 15 19"
+	cout << "Select is " << A.linearSearch(5) << endl;
+	// A => "10 0 1 2 3 4 5 6 7 8 9 15 19" Search => 6
+    for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
+	cout << "Select is " << A.QuickSelect(3) << endl;
+    for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
+
+    // Select => 2	
 	// cout << "Select is " << A.WCSelect(12) << endl;
-	// // Select => 15
-	// A.stableSort();
-	// // A => "0 1 2 3 4 5 6 7 8 9 10 15 19"
-	// A.addEnd(11); A.addFront(1); A.addFront(2); A.addFront(3);
-	// cout << "capacity is " << A.capacity() << endl;
-	// // A => "3 2 1 0 1 2 3 4 5 6 7 8 9 10 15 19 11"	  capacity => 32
-	// A.delFront(); A.delFront();
-	// // A => "1 0 1 2 3 4 5 6 7 8 9 10 15 19 11"	  capacity => 32
+	// Select => 15
+	A.stableSort();
+	// A => "0 1 2 3 4 5 6 7 8 9 10 15 19"
+	A.addEnd(11); A.addFront(1); A.addFront(2); A.addFront(3);
+	cout << "capacity is " << A.capacity() << endl;
+	// A => "3 2 1 0 1 2 3 4 5 6 7 8 9 10 15 19 11"	  capacity => 32
+    for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
 
-	// foo(A);
-	// for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
-	// // A => "1 0 1 2 3 4 5 6 7 8 9 10 15 19 11"
-	// for (int i=0; i< B.length();i++) cout << B[i] << " ";  cout << endl;
-	// // B => "0 1 2 3 4 5 6 7 8 9"
+	A.delFront(); A.delFront();
+	// A => "1 0 1 2 3 4 5 6 7 8 9 10 15 19 11"	  capacity => 32
+
+	foo(A);
+	for (int i=0; i< A.length();i++) cout << A[i] << " ";  cout << endl;
+	// A => "1 0 1 2 3 4 5 6 7 8 9 10 15 19 11"
+	for (int i=0; i< B.length();i++) cout << B[i] << " ";  cout << endl;
+	// B => "0 1 2 3 4 5 6 7 8 9"
 }
